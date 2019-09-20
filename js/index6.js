cfbce1e1ff19a6d72505e7bea0edd0a0b2141d42
Alex <olexandrmai@gmail.com>
2019-09-20 14:04:17 +0300

    cr animal and cat  class

---
+++ js/index6.js
@@ -0,0 +1,46 @@
+class Animal {
+    constructor(options){
+      this.name = options.name
+      this.age = options.age
+      this.hasTail = options.hasTail
+    }
+    
+    voice(){
+      console.log('I am animal') // добавляется в прототип
+    }
+  }
+  
+//   const animal = new Animal({//наследник класса Animal и его прототип
+//     name: "Animal",
+//     age: 7,
+//     hasTail: true
+//   });
+
+class Cat extends Animal {
+    constructor(options){
+        super(options)
+        this.color = options.color
+    }
+    voice(){
+        super.voice()
+        console.log('I am cat');
+      }
+
+    get ageInfo(){
+        return this.age * 7;
+    }
+
+    set ageInfo(newAge){
+        this.age = newAge;
+    }
+}
+  
+const cat = new Cat(
+    {
+    name: "Animal",
+    age: 7,
+    hasTail: true,
+    color: "black"
+    }
+);
+
--- js/reducer.js
+++ js/reducer.js
@@ -1,55 +1,79 @@
-// let state = 0;
+// // let state = 0;
 
-// function updateState (state, action){
-//     if(action === "Incriment"){
-//         return state + 1;
-//     } else if ( action === "Decriment"){
-//         return state - 1;
-//     } else {
-//         return state;
+// // function updateState (state, action){
+// //     if(action === "Incriment"){
+// //         return state + 1;
+// //     } else if ( action === "Decriment"){
+// //         return state - 1;
+// //     } else {
+// //         return state;
+// //     }
+// // }
+
+// // state = updateState(state, "Incriment")
+// // console.log(state);
+
+// // let state = 0;
+
+// function updateState(state, action) {
+//   if (action.type === "Incriment") {
+//     return state + action.amount;
+//   } else if (action.type === "Decriment") {
+//     return state - action.amount;
+//   } else {
+//     return state;
+//   }
+// }
+
+// class Store {
+//     constructor(updateState, state){
+//         this._updateState = updateState;
+//         this._state = state;
+//     }
+
+//     get state(){
+//         return this._state;
+//     }
+
+//     update(action){
+//         this._state = this._updateState(this._state, action);
 //     }
 // }
 
-// state = updateState(state, "Incriment")
-// console.log(state);
+// const store = new Store(updateState, 0);
 
-// let state = 0;
+// const inc = { type: "Incriment", amount: 5 };
+// const dec = { type: "Decriment", amount: 3 };
+
+// store.update(inc);
+// console.log(store.state);
+
+// store.update(dec);
+// console.log(store.state);
+
+// store.update({});
+// console.log(store.state);
+
+let state = 0;
 
 function updateState(state, action) {
-  if (action.type === "Incriment") {
+  if (action.type === "INCREMENT") {
     return state + action.amount;
-  } else if (action.type === "Decriment") {
+  } else if (action.type === "DECREMENT") {
     return state - action.amount;
   } else {
     return state;
   }
 }
 
-class Store {
-    constructor(updateState, state){
-        this._updateState = updateState;
-        this._state = state; 
-    }
+const incrementAction = {type: "INCREMENT", amount: 5};
+const decrementAction = {type: "DECREMENT", amount: 3};
 
-    get state(){
-        return this._state;
-    }
+state = updateState(state, incrementAction);
+console.log(state);
 
-    update(action){
-        this._state = this._updateState(this._state, action);
-    }
-}
+state = updateState(state, decrementAction);
+console.log(state);
 
-const store = new Store(updateState, 0); 
-
-const inc = { type: "Incriment", amount: 5 };
-const dec = { type: "Decriment", amount: 3 };
-
-store.update(inc);
-console.log(store.state);
-
-store.update(dec);
-console.log(store.state);
-
-store.update({});
-console.log(store.state);
+state = updateState(state, {});
+console.log(state);
