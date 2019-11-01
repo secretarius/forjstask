16250adf9832f4f45ad767b2139b8ba7dc9aa689
Alex <olexandrmai@gmail.com>
2019-11-01 22:59:25 +0200

    cr closure f and fizzbuzz

--- js/exampleDOM.js
+++ js/exampleDOM.js
@@ -23,3 +23,97 @@ const parent = document.getElementById('content');
 const firstChild = parent.childNodes[0];
 parent.insertBefore(p1, firstChild);
 parent.appendChild(p2);
+
+const element = document.getElementById('caption');
+console.log(element.innerHTML, element.innerText);
+element.addEventListener('click', event => {
+    console.dir({ event });
+});
+
+const elements = document.getElementsByClassName('panel1');
+//HTMLCollection, HTMLElement: Element
+console.log(elements);
+console.log(elements[0]);
+console.log(elements.namedItem('web'));
+console.log(elements['web']);
+
+
+
+
+//context closure
+const hash = () => {
+    const data = {};
+    let counter = 0;
+    return (key, value) => {
+        data[key] = value;
+        counter++;
+        console.log({counter});
+        return data;
+    }
+}
+
+const h1 = hash();
+h1('name','Alex');
+h1('age', 37);
+const obj1 = h1('job', false);
+console.log({obj1});
+//chain
+const hash2 = () => {
+    const data = {};
+    Object.defineProperty(data, 'add', {
+        enumerable: false,
+        value(key, value) {
+            data[key] = value;
+            return data;
+        }
+    });
+   return data;
+}
+
+console.log(
+    hash2().add('name','Alex')
+            .add('age', 37)
+            .add('job', false)
+);
+//closure
+const add1 = x => y => {
+    const z = x + y;
+    console.log(x + 'x' + y + '=' + z);
+    return z;
+};
+// const add1 = x => y => x + y;
+const res = add1(3)(6);
+console.log(res);
+//closure recursive
+const add2 = x => y => {
+    const z = x + y;
+    console.log(x + 'x' + y + '=' + z);
+    return add2(z);
+};
+
+//const add2 = x => y => add2(x + y);
+//usage
+const a1 = add2(5);
+const a2 = a1(2);
+const a3 = a2(3);
+const a4 = a1(1);
+const a5 = a2(10);
+console.log(a1, a2, a3, a4, a5);
+
+const res2 = add2(1)(4)(8)(8);
+console.log(res2);
+
+
+
+//fizzbazz
+for (let i = 1; i < 17; i++) {
+    if ( i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbazz");
+    } if ( i % 3 === 0 ) {
+        console.log("fizz");
+    } else if ( i % 5 === 0) {
+        console.log("bazz");
+    } else {
+        console.log(i);
+    }
+}
