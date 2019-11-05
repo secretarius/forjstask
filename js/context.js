fb8b2ecb42d9e2dd227a8fbab0cb08460fd62207
Alex <olexandrmai@gmail.com>
2019-11-05 20:54:16 +0200

    cr bind function

--- js/context.js
+++ js/context.js
@@ -36,3 +36,140 @@ console.log(person2.sayHello2());
 console.log(anotherPerson.sayHello2());
 // console.log(person2.sayHello2.call(anotherPerson)); // з функцією call;
 
+var user = {
+    name: "Alex",
+    
+	sayHallo: function() {
+		console.log("Say hello " + this.name);
+    }
+}
+
+setTimeout(user.sayHallo, 1000);// Say hello undefined
+
+// var f = user.sayHallo;
+// setTimeout(f, 1000); // Say hello undefined
+
+setTimeout(user.sayHallo.bind(user), 1000);// Say hello Alex
+
+//////////////// call, app;y, bind
+var employee1 = {
+    name: "Alex",
+    position: "Proect manager",
+    salary: 1000
+};
+
+var employee2 = {
+    name: "Olha",
+    position: "Junior developer",
+    salary: 800
+};
+
+var employee3 = {
+    name: "Yana",
+    position: "Meddle developer",
+    salary: 1000,
+};
+
+function promote(newPosition, salaryRise) {
+    this.position = newPosition;
+    this.salary += salaryRise;
+
+    return this.name + " is " + this.position + " and earns $" + this.salary;
+}
+//call
+console.log(promote.call(employee1, "Department head", 500));
+//apply
+console.log(promote.apply(employee2, ["Middle developer", 300]));
+//bind
+var promoteYana = promote.bind(employee3);
+console.log(promoteYana("Senior developer", 1500));
+var promoteYanaToSenior = promote.bind(employee3, "Senior developer");
+console.log(promoteYanaToSenior(1500));
+
+
+//////////////////////////////// bind
+function f(y) {
+    return this.x + y;
+}
+
+var o = {
+    x: 5,
+}
+
+// var g = f.bind(o);
+// console.log(g(2));
+
+function isBind (f, o) {
+    return function() {
+        return f.apply(o, arguments);
+        }
+    }
+
+var g = isBind(f, o);
+console.log(g(10));
+////////////////////////////////////////////
+const one = function(){
+
+    return this;
+    
+    }
+    
+    const two= one.bind({foo: "var"})
+    
+    const c = two.bind(null)
+    
+    console.log(c())
+
+/////////////////////////////////////////////////
+    function bind(func, context){
+
+        return function(...args) {
+            return func.apply(
+                        context,
+                        args
+                    );
+        }
+    }
+    
+    const a = function () {
+        return this;
+    }
+    
+    const b = bind(a, {foo: 'var' });
+    
+    const c = bind(b, null);
+    
+    const d = bind(c, { g: '1' });
+    
+    console.log(c())
+
+    /////////////////////
+var context = { 
+    foo: "bar" 
+};
+
+function returnFoo() {
+  return this.foo;
+}
+
+// console.log(returnFoo());//undefined
+
+var bound = returnFoo.bind(context);
+
+// Свойство теперь в области видимости
+console.log(bound()); 
+
+////////////////
+this.x = 9;
+var module = {
+  x: 81,
+  getX: function() { return this.x; }
+};
+
+console.log(module.getX());
+
+var getX = module.getX;
+console.log(getX()); 
+
+var boundGetX = getX.bind(module);
+console.log(boundGetX()); 
