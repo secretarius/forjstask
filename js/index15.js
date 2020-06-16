643ff9e229f1f8b37143d1d4b7e1763d60b45b9e
Alex <olexandrmai@gmail.com>
2020-06-16 18:31:28 +0300

    forEach by hand

--- js/index15.js
+++ js/index15.js
@@ -133,3 +133,35 @@ console.log(c);// [1, 2, 3, 4, 3, 4, 5, 6]
 
 for (var c=a, i=0; i<b.length; i++) a.push(b[i]);
 console.log(c);//[1, 2, 3, 4, 3, 4, 5, 6]
+
+//forEach by hand
+var each = function(array, callback, thisArg) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(thisArg, array[i], i, array);
+  }
+};
+var Person = function(name) {
+  this.name = name;
+} 
+
+Person.prototype.say = function(phrase) {
+  console.log(this.name + "say" + phrase);
+}
+
+// Person.prototype.mumble = function(phrases) {
+//   phrases.forEach(function(prase) {this.say(prase)}, this);
+// }
+
+Person.prototype.mumble = function(phrases) {
+  each(phrases, function(prase) {this.say(prase)}, this);
+}
+
+var jhonDou = new Person('Jhon Dou');
+jhonDou.mumble(['Hello, World!', 'JS is great', 'I\'m designer and i don\'t have job']);
+
+
+each([1,2,3], function(num, i, nums) {
+  console.log('Number: ' + num + ', index: ' + i + ',', nums);
+})
+
+[1, 2, 3, 4, 5].forEach(num => console.log(num * 2)); // 2 4 6 8 10
