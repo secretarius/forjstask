78d1343ff12bfecf248b5055cdb2db02f5cf0074
Alex <olexandrmai@gmail.com>
2020-06-16 14:11:31 +0300

    add exsemple marge array

--- js/index15.js
+++ js/index15.js
@@ -69,3 +69,67 @@ let size = arr => arr.length;
 let devide = (a, b) => a/b;
 let average = arr => devide(total(arr), size(arr));
 console.log(average(input));
+
+//array methods
+let arr = [1,3,4,5,6,78,3,2,3,5,67,78];
+
+let sum = arr.reduce((a,b) => a + b);
+console.log(sum);//255
+
+let arr3 = [1,2,3,true, false, undefined, null, NaN, "", "yess"];
+let newAr = arr3.filter(Boolean);
+console.log(newAr);//[1, 2, 3, true, "yess"]
+
+let cars = ["Audi", "BMW","Jeep","Opel","Citroen"];
+
+let arrSpred = [...cars];
+console.log(arrSpred)// ["Audi", "BMW", "Jeep", "Opel", "Cetroen"]
+
+let obSpred = {...cars};
+
+console.log(obj);//{0: "Audi", 1: "BMW", 2: "Jeep", 3: "Opel", 4: "Citroen"}
+
+let uni_n = [...new Set(arr)];
+console.log(uni_n);// [1, 3, 4, 5, 6, 78, 2, 67]
+
+let un = Array.from(new Set(arr));
+console.log(un);// [1, 3, 4, 5, 6, 78, 2, 67]
+
+let carss = [{ car: "Honda", color: "Red"},{ car: "Toyota", color: "Green"},{ car: "Seat", color: "Blue"},{ car: "BMW", color: "Red"},];
+let car_name = Array.from(carss, ({car}) => car);
+console.log(car_name);//["Honda", "Toyota", "Seat", "BMW"]
+
+arr.splice(0, arr.length);
+console.log(arr);// []
+
+let arrA = [1,3,4,5,6,78,3,2,3,5,67,78];
+let arrB = [1,0,4,6,3,5,63,78];
+
+let y = [...new Set(arrA)].filter(item => arrB.includes(item));
+console.log(y);//[1, 3, 4, 5, 6, 78]
+
+let last_index = arrA.lastIndexOf(3);
+console.log(last_index );//8
+
+let randomNum = arrA[(Math.floor(Math.random() * (arrA.length)))];
+console.log(randomNum);
+
+let slijan = obj.concat(car_name);
+console.log(slijan); //["Audi", "BMW", "Jeep", "Opel", "Citroen", "Honda", "Toyota", "Seat", "BMW"]
+
+let slijan2 = [...obj,...car_name];
+console.log(slijan2);//["Audi", "BMW", "Jeep", "Opel", "Citroen", "Honda", "Toyota", "Seat", "BMW"]
+
+let unik3 = [...new Set([...obj,...car_name])];
+console.log(unik3); //["Audi", "BMW", "Jeep", "Opel", "Cetroen", "Honda", "Toyota", "Seat"]
+
+
+var a = [1,2,3,4],
+b = [3,4,5,6],
+c;
+
+c = [...a, ...b];
+console.log(c);// [1, 2, 3, 4, 3, 4, 5, 6]
+
+for (var c=a, i=0; i<b.length; i++) a.push(b[i]);
+console.log(c);//[1, 2, 3, 4, 3, 4, 5, 6]
